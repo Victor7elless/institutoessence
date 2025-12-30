0377c5fc93793e4b005e879ffc975d77d1152fff
VituBababoe <victor7elles@gmail.com>
2025-12-30 11:53:50 -0400

    atualização2

--- src/componentes/welcome.tsx
+++ src/componentes/welcome.tsx
@@ -11,7 +11,7 @@ export default function Welcome() {
         <div className='fundo'>
           <h1>Bem-vindo(a) ao <br /> Instituto Essence</h1>
           <p>O Instituto Essence nasceu com o propósito de cuidar da saúde de forma integral, unindo ciência, terapias integrativas e um olhar humano para cada pessoa, com atendimentos individualizados e personalizados. 
-Mais do que uma clínica, é um espaço de acolhimento, equilíbrio e reconexão com a sua própria essência.
+<br />Mais do que uma clínica, é um espaço de acolhimento, equilíbrio e reconexão com a sua própria essência.
 Cada detalhe aqui foi pensado para promover bem-estar, saúde e qualidade de vida de dentro para fora.
 </p>
           <button>Saiba mais</button>
